caace395e5dab2f4bd28ac92a5c20d9071fe8803
Stephanie Livengood <sreneelive@gmail.com>
2023-06-08 15:38:52 -0400

    Finished adding all functionality to gather data from weather API to display on single page. Basic styling included.

--- app.js
+++ app.js
@@ -1,54 +1,78 @@
+//Stop form from submitting
+var form = document.getElementById("weatherForm");
+function handleForm(event) { event.preventDefault(); } 
+form.addEventListener('submit', handleForm);
 
 
-//let form = document.getElementById('weatherForm');
-
-    function getData(){
-        fetch("http://api.weatherstack.com/current?access_key=42b8d41c7809162091db1c8bc7ca52d4&query=Indiana", {mode:'cors'}).then(function(response){
-    return response.json()}).then(function(response){
-        DisplayWeather(response);
+//Call a function when btn is clicked
+let checkBtn = document.getElementById('check');
+checkBtn.addEventListener('click', function(){
+    
+    let locationState = check();
+    
+    fetch("http://api.weatherstack.com/current?access_key=42b8d41c7809162091db1c8bc7ca52d4&query=" + locationState, {mode:'cors'})
+    .then(function(response){
+        return response.json();
+        
     })
-    }
+        .then(function(response){
+            let location = response.location.name;
+            let temp = (response.current.temperature * 9/5) + 32;
+            let src = response.current.weather_icons;
+            DisplayWeather(location, temp, src);
+        })
+}) 
+
+        
+    
+    
 
     function check(){
         
-        let radio1 = document.getElementById('indiana');
-        let radio2 = document.getElementById('ohio');
-        let radio3 = document.getElementById('illinois');
-        let radio4 = document.getElementById('tennessee');
+        let radio1 = document.getElementById('indianapolis');
+        let radio2 = document.getElementById('phoenix');
+        let radio3 = document.getElementById('houston');
+        let radio4 = document.getElementById('chicago');
         if(radio1.checked){
-            location = "indiana";
-            getData();
+            return "indianapolis";
+            
             //DisplayWeather(location, response);
         } else if(radio2.checked){
-            location = "ohio";
-            getData();
+            return "phoenix";
+            
             //DisplayWeather(location, response);
         } else if(radio3.checked){
-            location = "illinois";
-            getData();
+            return "houston";
+            
             //DisplayWeather(location, response);
         } else if(radio4.checked){
-            location = "tennessee";
-            getData();
+            return "chicago";
+            
             //DisplayWeather(location, response);
         } else {
             console.log("No button selected");
         }
     }
 
-    function DisplayWeather(response){
-        console.log(response.location.name);
-        console.log(response.current.temperature);
+    function DisplayWeather(location, temp, src){
+        //clear form
+        let locationText = "";
+        let temperatureText = "";
+       let weatherImg = document.getElementById('weatherImg');
+       weatherImg.src = "";
+
+
+        //Display info in added elements
+       locationText = document.getElementById('locationText');
+       
+       temperatureText = document.getElementById('temperatureText');
+       
+       locationText.innerText = location;
+       temperatureText.innerText = temp + "°";
+       weatherImg.src = src;
+       
 
     }
 
-window.onload = init()
-function init(){
-    //this isn't working
-    let checkBtn = document.getElementById('check');
-    checkBtn.addEventListener = function(event){
-        event.preventDefault;
-        console.log("hi")
-        check();
-}
-}
+    
+
